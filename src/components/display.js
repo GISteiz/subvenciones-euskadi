745271ec8ec37bdedc0eb975b3e1f7aa765d59b3
Miguel Moreno <mmoreno.topografia@gmail.com>
2024-10-10 01:04:56 +0200

    feat: improve selection display

--- src/components/display.js
+++ src/components/display.js
@@ -1,14 +1,20 @@
 import * as htl from "npm:htl";
+import * as hp from "./helpers.js";
 
 export const Display = {
   grantSelection: function (selection) {
     if (selection) {
-      console.log(selection['oid'])
+      //console.log(selection['oid'])
       return htl.html`
-        <p>${selection['beneficiary_name']} | ${selection['beneficiary_id']}</p>
-        <p>${selection['name']}</p>
-        <p>${selection['convener_name']}</p>`
+        <div class="selection">
+          <h2 style="font-weight: bold">${selection['beneficiary_name']}</h2>
+          ${selection['beneficiary_id']}
+          <p>Subvencion: ${selection['name']}</p>
+          <p>Convocante: ${selection['convener_name']}</p>
+          <p>Cantidad: ${hp.numberToLocaleString(selection['granted_amount'])} €</p>
+          <p>Fecha: ${new Date(selection['granted_date']).toLocaleDateString("es-ES")}</p>
+        </div>`
     }
-    else { return '' }
+    else { return htl.html`<i>Selecciona una subvención para obtener más información clickando sobre su botón correspondiente a la izquerda de la línea.</i>` }
   }
 }
